713eae1048eb43714da5c517bf6917eaf3345b3e
Holger Piontek <hp9390@gmail.com>
2015-12-06 17:36:22 +0100

    Release v0.0.32

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('prefixgh', function () {
-    return gulp.src('gh-pages/dist/css/**/*.css')
+    return gulp.src('gh-pages/buid/css/**/*.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: true
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('gh-pages/dist'));
 });
